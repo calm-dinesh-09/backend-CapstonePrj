dddc21e64840ed62a8dd10d58c38b85feedc66ae
calm-dinesh-09 <dineshpunja6@gmail.com>
2024-12-21 12:46:08 +0530

    Update Backend-Server.py

--- Backend-Server.py
+++ Backend-Server.py
@@ -12,23 +12,23 @@
 
 import time
 import boto3
-#import mysql.connector
+import mysql.connector
 
 # Que URL
 queue_url = 'https://sqs.us-east-1.amazonaws.com/083351570286/BackendTrafficQueue'
 
 #Specify the database details
-host = 'database-1.cfioemrww3c1.ap-southeast-2.rds.amazonaws.com'
+host = 'healthcaredb.clwg1yhjslce.us-east-1.rds.amazonaws.com'
 user = 'admin'
-password = 'your password'
-database='demo'
+password = 'Test123456#'
+database='healthcaredb'
 
 #Create a SQS Client
 sqs = boto3.client('sqs', region_name='us-east-1', aws_access_key_id='AKIARG2BEA5XDZPJQN53', aws_secret_access_key='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')
 
 #Connect to the RDS MySQL Instance
-#mydb = mysql.connector.connect(host=’host’, user=’admin’, password=’password’, database=database)
-#mycursor = mydb.cursor()
+mydb = mysql.connector.connect(host=’host’, user=’admin’, password=’password’, database=database)
+mycursor = mydb.cursor()
 
 # Receive message from SQS queue
 response = sqs.receive_message(QueueUrl=queue_url)
@@ -51,10 +51,10 @@ address = customerDetailsList[1]
 
 #Write the record to the database
 
-#val = (name, address)
-#sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = (name, address)
+sql = "INSERT INTO customers (name, patientissue) VALUES (%s, %s)"
 
-#mycursor.execute(sql, val)
-#mydb.commit()
+mycursor.execute(sql, val)
+mydb.commit()
 
 print("Record inserted in the DB")
