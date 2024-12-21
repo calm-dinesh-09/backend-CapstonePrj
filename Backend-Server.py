f737903b2031e1d8c79b7c564e973f01d0d59468
calm-dinesh-09 <dineshpunja6@gmail.com>
2024-12-21 12:20:07 +0530

    Update Backend-Server.py

--- Backend-Server.py
+++ Backend-Server.py
@@ -24,7 +24,7 @@ password = 'your password'
 database='demo'
 
 #Create a SQS Client
-sqs = boto3.client('sqs')
+sqs = boto3.client('sqs', region_name='us-east-1', aws_access_key_id='AKIARG2BEA5XDZPJQN53', aws_secret_access_key='e18Ap0+TIkxtaUpMyZ+7eR+p51gdOze4nwPagOXO')
 
 #Connect to the RDS MySQL Instance
 #mydb = mysql.connector.connect(host=’host’, user=’admin’, password=’password’, database=database)
